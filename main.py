81f1fc5c3ccb61e24a2cc7bf2013a91a6a54f873
monsaity <kokoevdavid11@gmail.com>
2026-04-21 14:17:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from src.preprocess import main as preprocess_fighters_main
 
+from src.load_data import main as preprocess_fight_results_main
+
+from src.stats_analysis import main as preprocess_fight_stats_main
+
+from src.feature_engineering import main as feature_engineering_main
+
+
+
+def main():
+
+    preprocess_fighters_main()
+
+    preprocess_fight_results_main()
+
+    preprocess_fight_stats_main()
+
+    feature_engineering_main()
+
+    print("Data pipeline completed successfully.")
+
+
+
+if __name__ == "__main__":
+
+    main()
